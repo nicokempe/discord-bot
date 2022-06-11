8715eb6a5edaf7096537458c31e72057f7bffdd9
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-06-11 22:54:12 +0200

    Fix JSX syntax error and add the global footer

--- website/src/App.vue
+++ website/src/App.vue
@@ -3,7 +3,8 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <Footer></Footer>
+  <router-view></router-view>
 </template>
 
 <style>
