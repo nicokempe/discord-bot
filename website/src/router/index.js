04d88ad8ee07c5e82965668ec1e7922336ac6a15
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-06-16 05:46:56 +0200

    Add the landing view

--- website/src/router/index.js
+++ website/src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Landing from '../views/Landing.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Landing
   },
   // installation pages
   {
