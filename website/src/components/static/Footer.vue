ac6c7e8e12356ef92fc34f442c6e9fad00a8984d
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-06-19 05:46:43 +0200

    Add the footer content

--- website/src/components/static/Footer.vue
+++ website/src/components/static/Footer.vue
@@ -1,5 +1,14 @@
 <template>
     <footer>
-        [CONTENT]
+        <div class="legal-links">
+            <router-link to="/imprint">Imprint</router-link> - 
+            <router-link to="/privacy-policy">Privacy Policy</router-link> - 
+            <router-link to="/credits">Credits</router-link>
+        </div>
+        <div class="copyright">© 2022 Copyright Nico Kempe Einzelunternehmen</div>
     </footer>
 </template>
+
+<script>
+
+</script>
