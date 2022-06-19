c8465da70f289073e9f65236de2c8a0e0d4177f0
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-06-19 05:45:53 +0200

    Update the Landing page
    * Remove the logo from the view
    * Add a failed redirection text
    * Add the routes as consts
    * Add the automatic redirection in the onMounted method

--- website/src/views/Landing.vue
+++ website/src/views/Landing.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Logo" src="../assets/logo.png">
+    <p>If the redirection wont work please contact the support on the Discord server this panel belongs to.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+
+import router from '@/router';
+import { onMounted } from 'vue'
+
+const signedIn = false;
+const installFinished = true;
+const installStepAuthentication = true;
+const installStepLanguages = true;
+const installStepModules = true;
+
+const dashboardPath = '/dashboard';
+const installAuthViewPath = '/install/authentication';
+const installLangSelectViewPath = '/install/language-selection';
+const installModuleSelectViewPath = '/install/module-selection';
+const signInViewPath = '/sign-in';
+const forbiddenViewPath = '/403';
 
 export default {
   name: 'Landing',
-  components: {
-    // HelloWorld
+  setup () {
+    onMounted(() => {
+      if(installFinished) {
+        if(signedIn) {
+          router.push(dashboardPath)
+        } else {
+          router.push(signInViewPath);
+        }
+      } else if(installStepAuthentication) {
+        router.push(installAuthViewPath);
+      } else if(installStepLanguages) {
+        router.push(installLangSelectViewPath);
+      } else if(installStepModules) {
+        router.push(installModuleSelectViewPath);
+      } else {
+        router.push(forbiddenViewPath);
+      }
+    })
   }
 }
+
 </script>
