4b2001a341db4304ce038b2df37d83db3afd861c
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2022-06-19 06:01:55 +0200

    Remove unused style from the App
    * Remove the navigation style because it moved to the navbar components

--- website/src/App.vue
+++ website/src/App.vue
@@ -23,16 +23,4 @@ export default {
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
